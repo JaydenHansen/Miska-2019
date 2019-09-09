ababfdb6018de6a7f44acfd3e4c2ad38a83731c1
Sam <sammydeedge@hotmail.com>
2019-09-09 23:21:47 +1000

    Wwise Project
    NOTE: Some files removed
    
    Sound files:
    - River Ambience
    - Leaves in Wind
    - Early Evening Birds
    - Crickets in a field
    
    PLUGINS FOLDER
    GENERATED SOUNDBANKS FOLDER

---
+++ MISKA-Audio Wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,119 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB_BED = 3587600583U;
+        static const AkUniqueID EMIT_BIRDS = 1054363565U;
+        static const AkUniqueID EMIT_CICADAS = 1225233505U;
+        static const AkUniqueID EMIT_RIVER = 1862066479U;
+        static const AkUniqueID OBJ_BOTTLEPICKUP = 920825535U;
+        static const AkUniqueID OBJ_FOSSILPOI = 4108701605U;
+        static const AkUniqueID OBJ_ROCKCOLLIDE = 614124066U;
+        static const AkUniqueID OBJ_ROCKSTACKFALLS = 3472354586U;
+        static const AkUniqueID PLYR_FOOTSTEP = 4254456693U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace CIRCKET_LOC
+        {
+            static const AkUniqueID GROUP = 2805833147U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CRICKETFIELD = 4190162518U;
+                static const AkUniqueID CRICKETLAKE = 2840189495U;
+            } // namespace SWITCH
+        } // namespace CIRCKET_LOC
+
+        namespace CURRENTLOCATION
+        {
+            static const AkUniqueID GROUP = 3237332941U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DUCKS = 4114905825U;
+                static const AkUniqueID FIREFLIES = 1126065684U;
+                static const AkUniqueID FOX = 1083137540U;
+                static const AkUniqueID ROCKS = 1863277117U;
+                static const AkUniqueID STATION = 1010644047U;
+            } // namespace SWITCH
+        } // namespace CURRENTLOCATION
+
+        namespace LOCOMOTION_MATERIAL
+        {
+            static const AkUniqueID GROUP = 2739821022U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace LOCOMOTION_MATERIAL
+
+        namespace LOCOMOTION_SPEED
+        {
+            static const AkUniqueID GROUP = 2450193098U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SPRINT = 1296465089U;
+                static const AkUniqueID WALK = 2108779966U;
+            } // namespace SWITCH
+        } // namespace LOCOMOTION_SPEED
+
+        namespace TRASHTYPE
+        {
+            static const AkUniqueID GROUP = 1155297313U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BOTTLE = 3848582449U;
+            } // namespace SWITCH
+        } // namespace TRASHTYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CURRENTSCENEPROGRESS = 4175992265U;
+        static const AkUniqueID DISTANCEFROM_CENTRE = 546813746U;
+        static const AkUniqueID ROCKCOLLIDEFORCE = 1277399449U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BNK_DUCKS = 2516644323U;
+        static const AkUniqueID BNK_GLOBAL = 568373026U;
+        static const AkUniqueID BNK_ROCKS = 2733895951U;
+        static const AkUniqueID BNK_STATION = 1567831353U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__

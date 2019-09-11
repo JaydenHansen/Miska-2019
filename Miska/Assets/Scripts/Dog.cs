d993fd3169745f868bfb82b2f7d512dc039c443a
JohnPlant <johnplant1215@gmail.com>
2019-09-11 11:30:47 +1000

    Added fetch to the dog
    Uploaded new dog mesh
    Added ball script that handles throwing
    Updated Dog script to change the target between player, ball and base

--- Miska/Assets/Scripts/Dog.cs
+++ Miska/Assets/Scripts/Dog.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+enum Target
+{
+    Player,
+    Ball,
+    Base
+};
+
 [RequireComponent(typeof(NavMeshAgent))]
 public class Dog : MonoBehaviour
 {
     public Transform m_player;
+    public Ball m_ball;
     public Transform m_basePosition;
     public float m_radius;
     public float m_distanceBehindPlayer;
 
     NavMeshAgent m_agent;
+    Target m_currentTarget;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,28 +31,97 @@ public class Dog : MonoBehaviour
     
     void Update()
     {
-        NavMeshHit hit;    
-        if (NavMesh.SamplePosition(m_player.position, out hit, 1, NavMesh.AllAreas))
+        switch (m_currentTarget)
         {
-            float distance = (m_player.position - m_basePosition.position).sqrMagnitude;
-            if (distance <= m_radius * m_radius)
-            {                
-                m_agent.SetDestination(hit.position);
-            }
-            else
-            {
-                m_agent.SetDestination(m_basePosition.position);
-            }
+            case Target.Player:
+                {
+                    if (m_ball.Parent == Parent.None && (m_ball.transform.position - m_basePosition.position).sqrMagnitude < m_radius * m_radius)
+                    {
+                        m_currentTarget = Target.Ball;
+                        m_agent.stoppingDistance = 0;
+                        break;
+                    }
+                    float distance = (m_player.position - m_basePosition.position).sqrMagnitude;
+                    if (distance >= m_radius * m_radius)
+                    {
+                        m_currentTarget = Target.Base;
+                        m_agent.stoppingDistance = 0;
+                    }
+                    break;
+                }
+            case Target.Ball:
+                {
+                    if (m_ball.Parent == Parent.Dog)
+                    {
+                        m_currentTarget = Target.Player;
+                        m_agent.stoppingDistance = 5;
+                    }
+                    float distance = (m_ball.transform.position - m_basePosition.position).sqrMagnitude;
+                    if (distance >= m_radius * m_radius)
+                    {
+                        if ((m_player.position - m_basePosition.position).sqrMagnitude < m_radius * m_radius)
+                        {
+                            m_currentTarget = Target.Player;
+                            m_agent.stoppingDistance = 5;
+                        }
+                        else
+                        {
+                            m_currentTarget = Target.Base;
+                            m_agent.stoppingDistance = 0;
+                        }
+                    }
+                    break;
+                }
+            case Target.Base:
+                {
+                    if (m_ball.Parent == Parent.None)
+                    {
+                        float distance = (m_ball.transform.position - m_basePosition.position).sqrMagnitude;
+                        if (distance < m_radius * m_radius)
+                        {
+                            m_currentTarget = Target.Ball;
+                            m_agent.stoppingDistance = 0;
+                        }
+                    }
+                    else
+                    {
+                        float distance = (m_player.position - m_basePosition.position).sqrMagnitude;
+                        if (distance < m_radius * m_radius)
+                        {
+                            m_currentTarget = Target.Player;
+                            m_agent.stoppingDistance = 5;
+                        }
+                    }
+                    break;
+                }            
         }
-        else
+
+        NavMeshHit hit;
+        Vector3 targetPosition = Vector3.zero;
+        switch (m_currentTarget)
         {
-            m_agent.SetDestination(m_basePosition.position);
+            case Target.Player:
+                targetPosition = m_player.position;
+                break;
+            case Target.Ball:
+                targetPosition = m_ball.transform.position;
+                break;
+            case Target.Base:
+                targetPosition = m_basePosition.position;
+                break;
         }
+
+        m_agent.SetDestination(targetPosition);
+        //if (NavMesh.SamplePosition(targetPosition, out hit, 1, NavMesh.AllAreas))
+        //{
+        //    m_agent.SetDestination(hit.position);
+        //}
+        
 
         if (m_agent.velocity.sqrMagnitude != 0)
             transform.rotation = Quaternion.LookRotation(m_agent.velocity.normalized);
         else
-            transform.forward = (new Vector3(transform.position.x, 0, transform.position.z) - new Vector3(m_player.position.x, 0, m_player.position.z)).normalized;
+            transform.forward = (new Vector3(m_player.position.x, 0, m_player.position.z) - new Vector3(transform.position.x, 0, transform.position.z)).normalized;
     }
 
     public void Spawn()

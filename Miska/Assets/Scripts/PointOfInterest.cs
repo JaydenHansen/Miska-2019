c184a77688184e7554095d038401e4db93053d74
JohnPlant <johnplant1215@gmail.com>
2019-09-12 15:07:38 +1000

    Fixed point of interest
    Changed the Point of interest to use physics overlap box instead of its own collider

--- Miska/Assets/Scripts/PointOfInterest.cs
+++ Miska/Assets/Scripts/PointOfInterest.cs
@@ -5,31 +5,51 @@ using UnityEngine.UI;
 
 public class PointOfInterest : MonoBehaviour
 {
+    public GameObject m_player;
     public PopUp m_popUp;
     public Text m_trashCount;
     public TrashCan m_trashCan;
+    public Vector3 m_size;
 
-    private void OnTriggerEnter(Collider other)
+    bool m_playerInArea;
+
+    private void Update()
     {
-        if (other.tag == "Player")
+        bool playerInArea = false;
+        Collider[] hits = Physics.OverlapBox(transform.position, m_size * 0.5f, transform.rotation, 1 << LayerMask.NameToLayer("Player"));
+        foreach (Collider hit in hits)
+        {
+            if (hit.gameObject == m_player)
+            {
+                playerInArea = true;
+            }
+        }
+
+        if (!m_playerInArea && playerInArea)
         {
             m_popUp.StartPopUp(IconType.Rubbish, false);
             m_trashCount.text = m_trashCan.TrashLeft + "/" + m_trashCan.m_requiredTrash;
             m_trashCount.enabled = true;
         }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Player")
+        else if (m_playerInArea && !playerInArea)
+        {
+            m_popUp.StopPopUp();
+            m_trashCount.enabled = false;
+        }
+        else if (m_playerInArea && playerInArea)
         {
             m_trashCount.text = m_trashCan.TrashLeft + "/" + m_trashCan.m_requiredTrash;
         }
+
+        m_playerInArea = playerInArea;
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnDrawGizmosSelected()
     {
-        if (other.tag == "Player")
-            m_popUp.StopPopUp();
+        Gizmos.color = Color.green;
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+        Gizmos.DrawWireCube(Vector3.zero, m_size);
+        Gizmos.matrix = Matrix4x4.identity;
+        Gizmos.color = Color.white;
     }
 }

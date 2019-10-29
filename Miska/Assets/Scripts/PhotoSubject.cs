b94fe8e81beec87ff751cf1338f8fde2007df6c4
Jayden Hansen <jaydenhansendesign@gmail.com>
2019-10-30 08:58:09 +1100

    Main scene with grass changes

--- Miska/Assets/Scripts/PhotoSubject.cs
+++ Miska/Assets/Scripts/PhotoSubject.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using System.IO;
 
 public enum JournalSubject
-{
-    INACTIVE,
+{
+    INACTIVE,
     CLN_Station,
     CLN_Rocks,
     CLN_Ducks,
@@ -15,18 +15,18 @@ public enum JournalSubject
 
 [RequireComponent(typeof(Collider))]
 public class PhotoSubject : MonoBehaviour
-{
-    public bool m_isActive;
-
-    Collider m_coll;
-    public GameObject m_player;
-    bool isPlayerInPosition;
-
-
-    public Vector3 m_targetAngle;
-    public float m_validAngleOffset;
-    public RawImage m_poloroid;
-
+{
+    public bool m_isActive;
+
+    Collider m_coll;
+    public GameObject m_player;
+    bool isPlayerInPosition;
+
+
+    public Vector3 m_targetAngle;
+    public float m_validAngleOffset;
+    public RawImage m_poloroid;
+
     JournalSubject m_subject;
 
     string filename;
@@ -40,64 +40,64 @@ public class PhotoSubject : MonoBehaviour
         isPlayerInPosition = false;
     }
 
-    public JournalSubject getSubject()
-    {
-        return m_subject;
+    public JournalSubject getSubject()
+    {
+        return m_subject;
     }
 
-    public void SetActiveState(bool status) //Use to turn journal photo modes on and off
-    {
-        m_isActive = status;
+    public void SetActiveState(bool status) //Use to turn journal photo modes on and off
+    {
+        m_isActive = status;
     }
 
-    public bool GetActiveState()
-    {
-        return m_isActive;
+    public bool GetActiveState()
+    {
+        return m_isActive;
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.gameObject == m_player)
-        {
-            isPlayerInPosition = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.collider.gameObject == m_player)
-        {
-            isPlayerInPosition = false;
-        }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject == m_player)
+        {
+            isPlayerInPosition = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.gameObject == m_player)
+        {
+            isPlayerInPosition = false;
+        }
     }
     
-    public bool isPhotoValid()
-    {
-        if(!m_isActive)
-        {
-            return false;
-        }
-        else
-        {
-            Vector3 playerFacing = m_player.transform.forward;
-            bool isPlayerAngleValid = Vector3.Angle(playerFacing, m_targetAngle) < m_validAngleOffset;
-            return isPlayerInPosition && isPlayerAngleValid;
-        }
+    public bool isPhotoValid()
+    {
+        if(!m_isActive)
+        {
+            return false;
+        }
+        else
+        {
+            Vector3 playerFacing = m_player.transform.forward;
+            bool isPlayerAngleValid = Vector3.Angle(playerFacing, m_targetAngle) < m_validAngleOffset;
+            return isPlayerInPosition && isPlayerAngleValid;
+        }
     }
 
 
-    public void SetupPoloroid(string filename) //pass photo as an argument
-    {
-        Texture2D tex = null;
-        byte[] fileData;
-
-        if (File.Exists(filename))
-        {
-            fileData = File.ReadAllBytes(filename);
-            tex = new Texture2D(2, 2);
-            tex.LoadImage(fileData);
-        }
-
-        m_poloroid.texture = tex;
+    public void SetupPoloroid(string filename) //pass photo as an argument
+    {
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filename))
+        {
+            fileData = File.ReadAllBytes(filename);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData);
+        }
+
+        m_poloroid.texture = tex;
     }
-}
+}

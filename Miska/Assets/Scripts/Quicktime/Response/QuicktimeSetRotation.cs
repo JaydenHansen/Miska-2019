e84cd27ff5d5ff620ce4d1ec69803413af51fd24
JohnPlant <johnplant1215@gmail.com>
2019-11-07 10:35:16 +1100

    Stopped the player being able to jump over the log at different angles

---
+++ Miska/Assets/Scripts/Quicktime/Response/QuicktimeSetRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuicktimeSetRotation : QuicktimeResponse
+{
+    public Transform m_transform;
+    public Vector3 m_direction;
+    public bool m_localDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnSuccess()
+    {
+        m_transform.rotation = Quaternion.LookRotation(m_localDirection ? transform.rotation * m_direction : m_direction);
+    }
+}

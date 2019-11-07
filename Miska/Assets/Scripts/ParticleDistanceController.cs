750ea10106edb5a663b61f566169e4b98020f57b
JohnPlant <johnplant1215@gmail.com>
2019-11-07 16:04:55 +1100

    Added script to disable particles when they are too far away

---
+++ Miska/Assets/Scripts/ParticleDistanceController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleDistanceController : MonoBehaviour
+{
+    public Transform m_target;
+    public float m_distance;
+
+    bool m_active;
+    ParticleSystem m_particle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position - m_target.position).sqrMagnitude > m_distance * m_distance)
+        {
+            if (!m_active)
+            {
+                m_particle.Play();
+                m_active = true;
+            }
+        }
+        else
+        {
+            if (m_active)
+            {
+                m_particle.Stop();
+                m_active = false;
+            }
+        }
+    }
+}
